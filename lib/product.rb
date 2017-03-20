ce9b4d3ab43d681592548f4d9fa27dea8e133019
Nikolay <n.poymanov@gmail.com>
2017-03-20 22:14:26 +0300

    Добавлены базовые классы

---
+++ lib/product.rb
@@ -0,0 +1,7 @@
+class Product
+  def initialize(name, price, qty)
+    @name = name
+    @price = price
+    @qty = qty
+  end
+end
---
+++ main.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+require_relative "lib/product"
+require_relative "lib/film"
+require_relative "lib/book"
