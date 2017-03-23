7ca2a408fb63683163427fa19f542f32cc482923
Nikolay <n.poymanov@gmail.com>
2017-03-23 23:26:51 +0300

    Добавлен функционал покупки товаров

--- main.rb
+++ main.rb
@@ -6,17 +6,62 @@ require_relative "lib/film"
 require_relative "lib/book"
 require_relative "lib/disk"
 
-products = []
-
 current_path = File.dirname(__FILE__)
 
 products = ProductCollection.from_dir(current_path + "/data")
+products_array = products.to_a
+products_size = products_array.size
 
-# Сортировки
-# по цене
-products.sort!(by: :price)
+product_input = nil
 
-# по остатку
-products.sort!(by: :amount)
+user_products = []
+total = 0
 
-puts products.to_a
+until product_input == 0
+  puts "Что хотите купить:"
+  puts
+
+  # Вывод всех доступных товаров
+  products_array.each_with_index do |product, index|
+    puts "#{index + 1}. #{product}"
+  end
+
+  # Вариант для завершения покупки
+  puts "0. Выход"
+
+  product_input = STDIN.gets.to_i
+
+  # Если пользователь выбрал неверный вариант
+  unless product_input.between?(0, products_size)
+    next
+  end
+
+  # Завершение покупок
+  if product_input == 0
+    break
+  end
+
+  # Выбранный пользователем продукт
+  product_choice = products_array[product_input - 1]
+
+  puts
+  puts "Вы выбрали #{product_choice}"
+  puts
+
+  total += product_choice.price
+
+  puts "Всего товаров на сумму: #{total} руб."
+  puts
+
+  user_products << product_choice.to_s
+end
+
+puts "Вы купили:"
+puts
+
+user_products.each do |item|
+  puts item
+end
+
+puts
+puts "С Вас — #{total} руб. Спасибо за покупки!"
